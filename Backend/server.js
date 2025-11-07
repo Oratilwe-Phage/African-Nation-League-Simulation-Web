0c758ead45dcbc437318c630e45a3acf8b3e9cb6
Oratilwe-Phage <oraphage123@gmail.com>
2025-11-08 01:27:32 +0200

    fixed frontend dashbaord and backend server file issue

--- Backend/server.js
+++ Backend/server.js
@@ -50,9 +50,10 @@ app.use("/api/players", playerRoutes);
 app.use(express.static(path.join(__dirname, "public")));
 
 // ✅ Fallback for frontend routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 // ✅ 404 and error handlers
 app.use("/api/error", errorHandler);
